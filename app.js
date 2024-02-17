4a2ae13ee287cb9c7c46d076b9d106cab81f0a40
HP657 <junlee5070@gmail.com>
2024-02-18 06:55:22 +0900

    chore: Completed file and add :Comment

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+// JSON 형식을 쉽게 읽고 처리가능
 app.use(express.json());
 
+// 라우터링 가져오기
 const routes = require("./routes/router.js")
 app.use('/', routes)
 
--- routes/router.js
+++ routes/router.js
@@ -1,37 +1,40 @@
+// 라우터링
+
 const express = require("express");
 const router = express.Router();
 const db = require('./../DB/pool.js');
 const text_gpt = require("./../models/text_gpt.js");
 const img_gpt = require("./../models/img_gpt.js");
 
-
+// dream 테이블 조회
 router.get("/get_dream", async (req, res) => {
     const dream = await db.get_dream();
     res.json(dream);
 });
 
+// dream 테이블 추가
 router.put("/add_dream", async (req, res) => {
     const { title, content, writer } = req.body;
-    //const { title, content, writer } = req.params;
     await db.add_dream(title, content, writer);
     res.status(200).json();
 });
 
-router.delete("/del_dream/", async (req, res) => {
+// dream 테이블 삭제
+router.delete("/del_dream", async (req, res) => {
     const { title, writer } = req.body;
-    //const { title, writer } = req.params;
     await db.del_dream(title, writer);
     res.status(200).json();
 });
 
-
-router.post("/textgpt", async (req, res) => {
+// 텍스트 ChatGPT 사용
+router.post("/text_gpt", async (req, res) => {
     const text = req.body.text;
     const result = await text_gpt(text);
     res.json({text : result});
 });
 
-router.post("/imggpt", async (req, res) => {
+// 이미지 (DALL·E 2) 사용
+router.post("/img_gpt", async (req, res) => {
     const img = req.body.text;
     const result = await img_gpt(img);
     res.json({imgurl : result});
